80f01bb63e5036260179e5aa62c6fd4e448fa544
saransimkandisaran-png <saransimkandisaran@gmail.com>
2026-04-23 15:26:35 +0530

    Update │    └── MainClient.lua

--- Client/│    └── MainClient.lua
+++ Client/│    └── MainClient.lua
@@ -1,1 +1,14 @@
+local RS = game:GetService("ReplicatedStorage")
+local TrainEvent = RS:WaitForChild("TrainEvent")
 
+local UIS = game:GetService("UserInputService")
+
+-- Tap = Train
+UIS.InputBegan:Connect(function(input, processed)
+    if processed then return end
+
+    if input.UserInputType == Enum.UserInputType.Touch 
+    or input.UserInputType == Enum.UserInputType.MouseButton1 then
+        TrainEvent:FireServer()
+    end
+end)
